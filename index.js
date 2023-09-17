15bb1f16be2c5053fdf4283e202352d759f346da
Pedro Jaber <pedrojaberc@gmail.com>
2023-09-17 17:12:19 -0300

    adição das rotas de `candidato` e `representante`

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+const candidatoRouter = require("./routers/candidato");
+const representanteRouter = require("./routers/representante");
+
+//* App
 const app = express();
 const PORT = 8081;
 
-// middleware
+//* Middleware
 app.use(express.static("public"));
 
-// view engine
+//* View Engine
 app.use(expressLayouts);
 app.set("layout", "layouts/main");
 app.set("view engine", "ejs");
 
-// Dotenv
+//* Dotenv
 
-// Routes
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .render("paginaEmConstrucao", { title: "Página em construção" });
-});
+//* Routes
+app.get(["/", "/home"], (req, res) => res.status(200).send("Home"));
+// app.get("/", (req, res) => res.status(200).render("home", { title: "Home" }));
+app.use("/candidato", candidatoRouter);
+app.use("/representante", representanteRouter);
 
-// Error 404 Page
-app.get("*", (req, res) => {
-  res
-    .status(404)
-    .render("paginaEmConstrucao", { title: "Página em construção" });
-  // res.status(404).send("404");
-});
+//* Error 404 Page
+app.get("*", (req, res) => res.status(404).send("404"));
+// app.get("*", (req, res) =>
+//   res.status(404).render("404_error", { title: "Página não encontrada 404" }),
+// );
 
-// App Listen
+//* App Listen
 app.listen(PORT, () => {
   console.log("=~=~=~=~=~=~=~=~=~=~=~=~=~=");
   console.log("Server:\x1b[92m Online \x1b[0m");
