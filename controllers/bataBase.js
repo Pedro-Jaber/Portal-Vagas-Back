02c28f0c144a5a889ffb538fe823f1d92bc371c4
Pedro Jaber <pedrojaberc@gmail.com>
2023-10-01 18:03:48 -0300

    testes

--- controllers/bataBase.js
+++ controllers/bataBase.js
@@ -1,22 +1,36 @@
-const Candidato = require("../model/candidato");
-const Vaga = require("../model/vaga");
+const { Candidato, criarCandidato } = require("../model/candidato");
+const { Vaga } = require("../model/vaga");
+const {
+  VagaCandidato,
+  candidatoParticiparDeVaga,
+} = require("../model/vagaCandidato");
 
 module.exports.createCand = async (req, res) => {
   // criarCandidatos();
   // criarVagas();
-  res.send("criado");
+  // criarRelacaoVagaCandidato();
+
+  // const candidato = await criarCandidato(
+  //   "nomeTest",
+  //   "email@test.com",
+  //   "senhaTest",
+  // );
+  // res.send(candidato);
+
+  res.send("test");
 };
 
 module.exports.selectCand = async (req, res) => {
   const candidatos = await Candidato.findAll();
   const vagas = await Vaga.findAll();
+  const vagasCandidatos = await VagaCandidato.findAll();
 
-  res.send([candidatos, vagas]);
+  res.send([candidatos, vagas, vagasCandidatos]);
 };
 
 async function criarCandidatos() {
   try {
-    await Candidato.sync({ force: true }); // Isso irá criar a tabela 'candidato'
+    // await Candidato.sync({ force: true }); // Isso irá criar a tabela 'candidato'
 
     // Array de dados de candidatos
     const candidatosData = [
@@ -87,41 +101,57 @@ async function criarVagas() {
     // Array de dados de vagas
     const vagasData = [
       {
+        codigo: 1,
+        empresa: "Google",
         cargaHoraria: 20,
         bolsa: 800.0,
         descricao: "Estágio em Desenvolvimento Web",
       },
       {
+        codigo: 2,
+        empresa: "Microsoft",
         cargaHoraria: 30,
         bolsa: 1000.0,
         descricao: "Estágio em Machine Learning",
       },
       {
+        codigo: 3,
+        empresa: "Amazon",
         cargaHoraria: 25,
         bolsa: 900.0,
         descricao: "Estágio em Banco de Dados",
       },
       {
+        codigo: 4,
+        empresa: "Apple",
         cargaHoraria: 20,
         bolsa: 750.0,
         descricao: "Estágio em Desenvolvimento Mobile",
       },
       {
+        codigo: 5,
+        empresa: "Facebook",
         cargaHoraria: 15,
         bolsa: 700.0,
         descricao: "Estágio em Desenvolvimento Front-End",
       },
       {
+        codigo: 6,
+        empresa: "IBM",
         cargaHoraria: 35,
         bolsa: 1200.0,
         descricao: "Estágio em Segurança da Informação",
       },
       {
+        codigo: 7,
+        empresa: "Intel",
         cargaHoraria: 20,
         bolsa: 900.0,
         descricao: "Estágio em Ciência de Dados",
       },
       {
+        codigo: 8,
+        empresa: "Oracle",
         cargaHoraria: 25,
         bolsa: 800.0,
         descricao: "Estágio em Testes de Software",
@@ -135,4 +165,41 @@ async function criarVagas() {
   } catch (error) {
     console.error("Erro ao criar vagas:", error);
   }
+}
+
+async function criarRelacaoVagaCandidato() {
+  candidatoParticiparDeVaga(
+    "23f65e91-8bf5-41c5-b1db-464168db6f5d",
+    "6b9f0c3b-de03-4839-af69-43ccde64f716",
+  );
+
+  candidatoParticiparDeVaga(
+    "0e0667dc-92b1-4d66-a11c-aa84a4f44573",
+    "25c6ace2-fd86-4cc4-841c-bcc9194c3c6a",
+  );
+
+  candidatoParticiparDeVaga(
+    "f3047d16-18fd-47f6-8401-1082683957c6",
+    "59fd0395-8a48-485c-a45d-0cc4ee14aa4c",
+  );
+
+  candidatoParticiparDeVaga(
+    "0aa42b0e-e885-45c7-a808-48bc7cb6ff00",
+    "5b07e8ca-8e84-433d-86ac-f39b0a714b5e",
+  );
+
+  candidatoParticiparDeVaga(
+    "4b65c044-bdca-4fce-84c0-ad09c89c14da",
+    "60132ea8-65ab-419b-b26e-67451ccdfd0d",
+  );
+
+  candidatoParticiparDeVaga(
+    "1b0af4cb-b81b-4a02-877a-851370040fa9",
+    "63acb124-e426-4226-93c5-464ca6a967bc",
+  );
+
+  candidatoParticiparDeVaga(
+    "15d5424e-e1c9-4ee6-97fd-9b01de4f5b0e",
+    "da8b8fed-ac9d-4cb9-9545-f4af558909b8",
+  );
 }
